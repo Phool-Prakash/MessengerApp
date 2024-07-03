208bc3478f88fdddbbddbec93637cec8b8b615dd
Phool Prakash <phoolprakash62@gmail.com>
2024-07-03 09:11:46 +0530

    Animate the Transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_app/animations/animations.dart';
 import 'package:messenger_app/destinations/destinations.dart';
 import 'package:messenger_app/model/data.dart' as data;
+import 'package:messenger_app/widgets/animated_floating_action_button.dart';
+import 'package:messenger_app/widgets/disappearing_bottom_navigation_bar.dart';
 import 'package:messenger_app/widgets/disappearing_navigation_rail.dart';
 import 'package:messenger_app/widgets/email_list_view.dart';
 import 'model/models.dart';
@@ -36,14 +39,24 @@ class Feed extends StatefulWidget {
   State<Feed> createState() => _FeedState();
 }
 
-class _FeedState extends State<Feed> {
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
   late final _colorScheme = Theme.of(context).colorScheme;
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  late final _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      reverseDuration: const Duration(milliseconds: 1250),
+      value: 0,
+      vsync: this);
+
+  late final _railAnimation = RailAnimation(parent: _controller);
+  late final _railFabAnimation = RailFabAnimation(parent: _controller);
+  late final _barAnimation = BarAnimation(parent: _controller);
+
   int selectedIndex = 0;
 
-  bool wideScreen = false;
+  bool controllerInitialized = false;
 
   @override
   void didChangeDependencies() {
@@ -51,62 +64,81 @@ class _FeedState extends State<Feed> {
     super.didChangeDependencies();
 
     final double width = MediaQuery.of(context).size.width;
-    wideScreen = width > 600;
+
+    final AnimationStatus status = _controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        _controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        _controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      _controller.value = width > 600 ? 1 : 0;
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Row(
-          children: [
-            if (wideScreen)
-              DisappearingNavigationRail(
-                backgroundColor: _backgroundColor,
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (index) {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-              ),
-            Expanded(
-              child: Container(
-                  color: _backgroundColor,
-                  child: EmailListView(
-                    selectedIndex: selectedIndex,
-                    onSelected: (index) {
-                      setState(() {
-                        selectedIndex = index;
-                      });
-                    },
-                    currentUser: widget.currentUser,
-                  )),
+    return AnimatedBuilder(
+        animation: _controller,
+        builder: (context, _) {
+          return Scaffold(
+            body: Row(
+              children: [
+                DisappearingNavigationRail(
+                  backgroundColor: _backgroundColor,
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  railAnimation: _railAnimation,
+                  railFabAnimation: _railFabAnimation,
+                ),
+                Expanded(
+                  child: Container(
+                      color: _backgroundColor,
+                      child: EmailListView(
+                        selectedIndex: selectedIndex,
+                        onSelected: (index) {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                        },
+                        currentUser: widget.currentUser,
+                      )),
+                ),
+              ],
             ),
-          ],
-        ),
-        floatingActionButton: wideScreen
-            ? null
-            : FloatingActionButton(
-                backgroundColor: _colorScheme.tertiaryContainer,
-                foregroundColor: _colorScheme.onTertiaryContainer,
-                onPressed: () {},
-                child: const Icon(Icons.add),
-              ),
-        bottomNavigationBar: wideScreen
-            ? null
-            : NavigationBar(
-                elevation: 0,
-                backgroundColor: Colors.white,
-                destinations: destinations.map<NavigationDestination>((d) {
-                  return NavigationDestination(
-                      icon: Icon(d.icon), label: d.label);
-                }).toList(),
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (index) {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-              ));
+            floatingActionButton: AnimatedFloatingActionButton(
+              animation: _barAnimation,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+            bottomNavigationBar: DisappearingBottomNavigationBar(
+              barAnimation: _barAnimation,
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          );
+        });
   }
 }
